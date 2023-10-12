5b3a60d27856182d3a1d3a4926b1bd4442c30616
Anderson Menezes <anderson.stifip@gmail.com>
2023-10-12 02:51:12 -0300

    Commit initial - class 01, 02 e 03

---
+++ src/main/java/com/andersonmenezes/apispring/filter/FilterTaskAuth.java
@@ -0,0 +1,63 @@
+package com.andersonmenezes.apispring.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.andersonmenezes.apispring.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+        // Pegar a autenticação
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecoded);
+
+        // ["andersonmenezes", "135791"]
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println(username);
+        System.out.println(password);
+
+        // Validar usuário
+        var user = this.userRepository.findByUsername(username);
+        
+        if(user == null) {
+            response.sendError(401);
+        } else {
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            } 
+        }
+    }
+
+    
+}
